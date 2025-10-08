83116acf90586725f66b8828a49ba51ca7554e06
wlrnjs <tjwlrnjs7336@icloud.com>
2025-10-09 00:18:36 +0900

    주석 제거

--- src/tokens/gen-alias.js
+++ src/tokens/gen-alias.js
@@ -1,4 +1,3 @@
-// src/tokens/gen-alias.js
 import fs from "node:fs";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
--- src/tokens/sd-tokens-config.js
+++ src/tokens/sd-tokens-config.js
@@ -5,8 +5,8 @@ register(StyleDictionary);
 
 const sd = new StyleDictionary({
   source: [
-    "src/tokens/sd-*.json",              // split 산출물 3개
-    "src/tokens/sd-alias.generated.json" // ✅ 매 빌드마다 자동 생성
+    "src/tokens/sd-*.json",
+    "src/tokens/sd-alias.generated.json"
   ],
   log: { verbosity: 'verbose' }, // 누가 참조 깨졌는지까지 출력
   preprocessors: ["tokens-studio"],
